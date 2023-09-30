d178b02fbb1acf144daa9bb1a923e4f8058e8f61
Laurat Hajrullaaga <78878634+laurathaga@users.noreply.github.com>
2023-09-30 23:10:19 +0200

    Adding comment

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -2,6 +2,7 @@ function InitColors(color)
   color = color or 'rose-pine';
   vim.cmd.colorscheme(color);
 
+  -- set background to transparent
   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
