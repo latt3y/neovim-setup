5abf8bd93756130a7f4996385217aa1dcdbca497
Laurat Hajrullaaga <78878634+laurathaga@users.noreply.github.com>
2023-09-30 18:07:59 +0200

    Delete nvim/after/telescope.lua

--- nvim/after/telescope.lua
+++
@@ -1,7 +0,0 @@
-local builtin = require("telescope.builtin")
-
-vim.keymap.set("n", "<leader>pf", builin.find_files, {})
-vim.keymap.set("n", "<C-v>", builin.git_files, {})
-vim.keymap.set("n", "<leader>ps", function() 
-    builtin.grep_string({ string = vim.fn.input("Grep > ") });
-end)
