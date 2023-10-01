94946c4424001e3e68893de5ea0684bf4150fe38
Laurat Hajrullaaga <78878634+laurathaga@users.noreply.github.com>
2023-10-01 16:50:07 +0200

    Update keymap.lua

--- nvim/lua/lati/keymap.lua
+++ nvim/lua/lati/keymap.lua
@@ -4,6 +4,10 @@ vim.g.mapleader = " "
 
 keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- When searching for terms, keep cursor in the middle of the editor
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
 -- copy to system clipboard
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
