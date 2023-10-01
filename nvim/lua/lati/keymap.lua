c5cb6dd48ace298fb4614832c2331824fc72f339
Laurat Hajrullaaga <78878634+laurathaga@users.noreply.github.com>
2023-10-01 16:49:42 +0200

    Update keymap.lua

--- nvim/lua/lati/keymap.lua
+++ nvim/lua/lati/keymap.lua
@@ -3,3 +3,7 @@ local keymap = vim.keymap
 vim.g.mapleader = " "
 
 keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- copy to system clipboard
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
