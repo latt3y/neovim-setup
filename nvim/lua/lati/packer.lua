afc7cb75d1ed10ffe2f9123b113bfb1d0be7f0f4
Laurat Hajrullaaga <78878634+laurathaga@users.noreply.github.com>
2023-10-01 13:31:45 +0200

    Updating LSP

--- nvim/lua/lati/packer.lua
+++ nvim/lua/lati/packer.lua
@@ -10,20 +10,20 @@ return require('packer').startup(function(use)
       requires = { {'nvim-lua/plenary.nvim'} }
     }
 
-    -- Language syntax protocol
     use {
       'VonHeikemen/lsp-zero.nvim',
-      branch = 'v2.x',
+      branch = 'v3.x',
       requires = {
+        --- Uncomment these if you want to manage LSP servers from neovim
+        {'williamboman/mason.nvim'},
+        {'williamboman/mason-lspconfig.nvim'},
+    
         -- LSP Support
-        {'neovim/nvim-lspconfig'},             -- Required
-        {'williamboman/mason.nvim'},           -- Optional
-        {'williamboman/mason-lspconfig.nvim'}, -- Optional
-  
+        {'neovim/nvim-lspconfig'},
         -- Autocompletion
-        {'hrsh7th/nvim-cmp'},     -- Required
-        {'hrsh7th/cmp-nvim-lsp'}, -- Required
-        {'L3MON4D3/LuaSnip'},     -- Required
+        {'hrsh7th/nvim-cmp'},
+        {'hrsh7th/cmp-nvim-lsp'},
+        {'L3MON4D3/LuaSnip'},
       }
     }
 
