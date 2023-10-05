84f1b66f6fbb96e83d0e48153eacd8ce3b357605
Laurat Hajrullaaga <78878634+laurathaga@users.noreply.github.com>
2023-10-05 19:34:47 +0200

    Ignore node_modules for telescope

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,4 +1,7 @@
+local telescope = require("telescope")
 local builtin = require("telescope.builtin")
+
+telescope.setup{ defaults = { file_ignore_patterns = {"node%_modules/.*"} } }
 
 vim.keymap.set("n", "<leader>pf", builin.find_files, {})
 vim.keymap.set("n", "<C-v>", builin.git_files, {})
