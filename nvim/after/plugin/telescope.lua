3d84b09b097f45e259a0c734072a2a882f03116f
Laurat Hajrullaaga <78878634+laurathaga@users.noreply.github.com>
2023-10-05 19:35:57 +0200

    fixing typo

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -3,8 +3,8 @@ local builtin = require("telescope.builtin")
 
 telescope.setup{ defaults = { file_ignore_patterns = {"node%_modules/.*"} } }
 
-vim.keymap.set("n", "<leader>pf", builin.find_files, {})
-vim.keymap.set("n", "<C-v>", builin.git_files, {})
+vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+vim.keymap.set("n", "<C-v>", builtin.git_files, {})
 vim.keymap.set("n", "<leader>ps", function() 
     builtin.grep_string({ string = vim.fn.input("Grep > ") });
 end)
