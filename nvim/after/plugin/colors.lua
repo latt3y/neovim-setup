1d85197bdd5f4e8b189656ada4dd6fdd57dea1c5
Laurat Hajrullaaga <78878634+laurathaga@users.noreply.github.com>
2023-11-12 17:01:02 +0100

    Update colors.lua

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,10 +1,11 @@
 function InitColors(color) 
-  color = color or 'rose-pine';
+  -- color = color or 'rose-pine';
+  color = color or 'gruvbox';
   vim.cmd.colorscheme(color);
 
   -- set background to transparent
-  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 InitColors()
