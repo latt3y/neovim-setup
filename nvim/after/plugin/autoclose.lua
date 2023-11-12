9f295bf2018186fa6f7ed782b1cd968ddbdce1aa
Laurat Hajrullaaga <78878634+laurathaga@users.noreply.github.com>
2023-11-12 17:01:15 +0100

    Update autoclose.lua

--- nvim/after/plugin/autoclose.lua
+++ nvim/after/plugin/autoclose.lua
@@ -1,5 +1,5 @@
 require("autoclose").setup({
    keys = {
-      ["$"] = { escape = true, close = true, pair = "$$", disabled_filetypes = {} },
+      ["$"] = { escape = true, close = true, pair = "$", disabled_filetypes = {} },
    },
 })
