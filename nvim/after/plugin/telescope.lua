0470595c07f93e767a52842fe44598361979cb1d
Laurat Hajrullaaga <78878634+laurathaga@users.noreply.github.com>
2024-02-11 17:28:24 +0100

    Update telescope.lua

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -6,6 +6,4 @@ telescope.setup{ defaults = { file_ignore_patterns = {"node%_modules/.*"} } }
 vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.git_files, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set("n", "<leader>ps", function()
-    builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end)
+vim.keymap.set("n", "<leader>fw", builtin.grep_string, {});
