5b15ea2ca51654f0774e48a11832d9912160da76
Laurat Hajrullaaga <78878634+laurathaga@users.noreply.github.com>
2024-02-11 17:39:33 +0100

    Adding comment plugin

--- nvim/lua/lati/packer.lua
+++ nvim/lua/lati/packer.lua
@@ -14,6 +14,8 @@ return require('packer').startup(function(use)
       requires = { {'nvim-lua/plenary.nvim'} }
     }
 
+    use { 'numToStr/Comment.nvim' }
+
     use {
       'VonHeikemen/lsp-zero.nvim',
       branch = 'v3.x',
