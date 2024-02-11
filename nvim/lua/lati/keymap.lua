1d01ff57ae4dff9c4c7f1f768494f04161d7afff
Laurat Hajrullaaga <78878634+laurathaga@users.noreply.github.com>
2024-02-11 16:07:10 +0100

    Update keymap.lua

--- nvim/lua/lati/keymap.lua
+++ nvim/lua/lati/keymap.lua
@@ -12,6 +12,15 @@ keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
+-- window movement
+vim.keymap.set("n", '<leader>wl', '<C-w>v')
+vim.keymap.set("n", '<leader>wb', '<C-w>s')
+vim.keymap.set("n", '<leader>wc', '<C-w>c')
+vim.keymap.set("n", '<leader>l', '<C-w>l')
+vim.keymap.set("n", '<leader>h', '<C-w>h')
+vim.keymap.set("n", '<leader>j', '<C-w>j')
+vim.keymap.set("n", '<leader>k', '<C-w>k')
+
 -- copy to system clipboard
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
