dbbaf7bfdcf7f867b24739f5b79f1e8156baede6
= <=>
2025-01-01 01:04:52 +0100

    seting up lazy

--- init.lua
+++ init.lua
@@ -1,3 +1,11 @@
-print('New setup baby')
+require("lazy");
 
+vim.keymap.set("n", "<space><space>", "<cmd>source %<CR>")
 
+vim.api.nvim_create_autocmd('TextYankPost', {
+   desc = "Highlight when yanking text",
+   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+   callback = function() 
+      vim.highlight.on_yank();
+   end
+});
